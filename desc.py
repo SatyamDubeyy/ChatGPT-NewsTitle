d886174c43454cd3e34461ae85a5474f356488f6
SatyamDubeyy <98874638+SatyamDubeyy@users.noreply.github.com>
2023-06-04 11:34:28 +0530

    desc.py file exception handling

--- desc.py
+++ desc.py
@@ -4,5 +4,9 @@ def extract_desc(url):
     r=requests.get(url)
     soup=BeautifulSoup(r.text,"lxml")
     head=soup.find("head")
-    desc=head.find("meta",attrs={'name':'description'}).get("content")
+    try:
+        desc=head.find("meta",attrs={'name':'description'}).get("content")
+    except Exception as e:
+        desc=""
+        print(e)
     return desc
